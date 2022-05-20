57397f77c7c5f8b9f8f0086a225f50365e63157f
Valentin Chauveau <valentin.korea@gmail.com>
2022-05-20 11:29:12 +0200

    new routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :lists, only: [:index, :show, :new, :create] do
-    resources :bookmarks, only: [:index, :show, :new, :create]
+  root to: 'lists#index'
+  resources :lists, only: [:show, :new, :create] do
+    resources :bookmarks, only: [:show, :new, :create]
   end
   resources :bookmarks, only: [:destroy]
 end
